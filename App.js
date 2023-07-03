d2e0ec5e7b70f2be6b982f9991efa7c0731d7ad1
SrHuevardo <nicolasfsacco@gmail.com>
2023-07-03 20:37:13 -0300

    correccion menor

--- App.js
+++ App.js
@@ -29,29 +29,3 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
 });
-
-
-
-
-
-
-// import { StatusBar } from 'expo-status-bar';
-// import { SafeAreaView, StyleSheet, } from 'react-native';
-// import React from 'react';
-// import Amazona from './src/amazona';
-
-// export default function App() {
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <Amazona />
-//       <StatusBar style="auto" />
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//   },
-// });
