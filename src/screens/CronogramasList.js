81b29fb637ddb640710bf328b7ddc1e45c259373
SrHuevardo <nicolasfsacco@gmail.com>
2023-10-04 21:23:03 -0300

    todas las cards y estetica

--- src/screens/CronogramasList.js
+++ src/screens/CronogramasList.js
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
-import { StyleSheet, ScrollView } from 'react-native'
-import { SafeAreaView } from 'react-navigation'
-import {
-  useNavigation,
-  useRoute,
-  useFocusEffect,
-} from '@react-navigation/native'
-import { Banner } from 'react-native-paper'
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { useNavigation, useRoute, useFocusEffect } from '@react-navigation/native';
+import { Banner } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient'; 
 
-import { SCHEDULE_CREATE } from '../strings'
-import CreateFAB from '../components/CreateFAB'
-import CronogramaListItem from '../components/CronogramaListItem'
-import { openDatabase, getSchedules } from '../model'
+import { SCHEDULE_CREATE } from '../strings';
+import CreateFAB from '../components/CreateFAB';
+import CronogramaListItem from '../components/CronogramaListItem';
+import { openDatabase, getSchedules } from '../model';
 
 function addCronogramaAction(navigation) {
-  navigation.navigate(SCHEDULE_CREATE)
+  navigation.navigate(SCHEDULE_CREATE);
 }
 
 function CronogramasList() {
-  const [schedules, setSchedules] = useState([])
-  const [bannerVisible, setBannerVisible] = useState(true)
-  const navigation = useNavigation()
-  const route = useRoute()
-  let action = false
+  const [schedules, setSchedules] = useState([]);
+  const [bannerVisible, setBannerVisible] = useState(true);
+  const navigation = useNavigation();
+  const route = useRoute();
+  let action = false;
   if (route.params && route.params.action) {
-    action = route.params.action
+    action = route.params.action;
   }
   useFocusEffect(
     React.useCallback(() => {
-      const db = openDatabase()
-      getSchedules(db, setSchedules)
+      const db = openDatabase();
+      getSchedules(db, setSchedules);
     }, []),
-  )
+  );
 
   return (
-    <SafeAreaView style={styles.container}>
+    <LinearGradient
+      colors={['rgb(207,226,136)', 'rgb(45,105,129)']}
+      style={styles.container}
+    >
       {action && (
         <Banner
           theme={{ colors: { primary: 'green' } }}
@@ -54,14 +54,14 @@ function CronogramasList() {
         ))}
       </ScrollView>
       <CreateFAB onPress={() => addCronogramaAction(navigation)} />
-    </SafeAreaView>
-  )
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-})
+});
 
-export default CronogramasList
+export default CronogramasList;
